a7462a92b05908ea715a858609e93ffc19300d5a
Vinay Kumar <vinaydagar514@gmail.com>
2025-02-08 01:50:39 +0530

    Added image animation in Homepage

--- app/layout.js
+++ app/layout.js
@@ -1,50 +1,48 @@
 import { Inter } from "next/font/google";
-
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
 import { dark } from "@clerk/themes";
 
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata = {
-  title: "Smartai - AI Career Coach",
+  title: " Smart - AI Career Coach",
   description: "",
 };
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <ClerkProvider
-        appearance={{
-          baseTheme: dark,
-        }}
-      >
-        <html lang="en" suppressHydrationWarning>
-          <head>
-            <link rel="icon" href="/logo.png" sizes="any" />
-          </head>
-          <body className={`${inter.className}`}>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem
-              disableTransitionOnChange
-            >
-              {/* header */}
-              <Header />
-              <main className="min-h-screen">{children}</main>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
 
-              {/* footer */}
-              <footer className="bg-muted/50 py-12">
-                <div className="container mx-auto px-4 text-center text-gray-200">
-                  <p>Made with Vinay Kumarr</p>
-                </div>
-              </footer>
-            </ThemeProvider>
-          </body>
-        </html>
-      </ClerkProvider>
-    </>
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with 💗 by RoadsideCoder</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="grid-background"></div>;
+      <HeroSection />
+    </div>
+  );
+}
