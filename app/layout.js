11d43b25f8e3fe4b308cd512c5c196d7fb043071
Vinay Kumar <vinaydagar514@gmail.com>
2025-02-08 21:50:01 +0530

    addded Landing Page

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by RoadsideCoder</p>
+                <p>Made with 💗 by Vinay Kumar</p>
               </div>
             </footer>
           </ThemeProvider>
