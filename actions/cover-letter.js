718fd0f4f078271f66596ea24693435e6cd44695
Vinay Kumar <vinaydagar514@gmail.com>
2025-04-27 21:03:56 +0530

    Fix case sensitivity in cover letter retrieval function

--- actions/cover-letter.js
+++ actions/cover-letter.js
@@ -95,7 +95,7 @@ export async function getCoverLetter(id) {
 
   if (!user) throw new Error("User not found");
 
-  return await db.coverLetter.findUnique({
+  return await db.CoverLetter.findUnique({
     where: {
       id,
       userId: user.id,
